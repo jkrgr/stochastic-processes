79a700fd7c96bd593b01fb0cf05d42a5c371bd6e
jk <jk@redpai.no>
2012-10-29 16:39:54 +0100

    ...

--- branchtrials.m
+++ branchtrials.m
@@ -47,17 +47,17 @@ std(largest_gens)
 
 %f=plot_histograms(gen_sizes, pop_sizes, largest_gens, N);
 
-[n1, xout1] = hist(gen_sizes,25);
+[n1, xout1] = hist(gen_sizes,40);
 bar(xout1, n1/N);
 figure(1);
 print('-dpdf','gen.pdf');
 
-[n2,xout2] = hist(pop_sizes, 25);
+[n2,xout2] = hist(pop_sizes, 40);
 bar(xout2,n2/N);
 figure(2);
 print('-dpdf','pop.pdf');
 
-[n3, xout3] = hist(largest_gens, 25);
+[n3, xout3] = hist(largest_gens, 40);
 bar(xout3,n3/N);
 figure(3);
 print('-dpdf','largest.pdf');
