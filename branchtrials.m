360ea7cec3c44964df688c0bd599430eb0213c9f
Joachim Kruger <joachibe@markov.math.ntnu.no>
2012-10-29 16:21:47 +0100

    figs

--- branchtrials.m
+++ branchtrials.m
@@ -45,5 +45,21 @@ disp('(iii)  Maximum number of individuals')
 mean(largest_gens)
 std(largest_gens)
 
-f=plot_histograms(gen_sizes, pop_sizes, largest_gens, N);
+%f=plot_histograms(gen_sizes, pop_sizes, largest_gens, N);
+
+[n1, xout1] = hist(gen_sizes,25);
+bar(xout1, n1/N);
+figure(1);
+print -depsc2 gen_sizes.eps;
+
+[n2,xout2] = hist(pop_sizes, 25);
+bar(xout2,n2/N);
+figure(2);
+print -depsc2 pop_sizes.eps;
+
+[n3, xout3] = hist(largest_gens, 25);
+bar(xout3,n3/N);
+figure(3);
+print -depsc2 largest_gens.eps;
+
 end
